b8d4ce04a6dc509b94c5fa683e13d399fcc8bd5e
jezzicaaaaa <jezzicamae@gmail.com>
2020-09-09 16:35:38 -0400

    first commit, established folder structures and components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: [ './recipe-list.component.css' ]
+})
+export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe(
+			'A Test recipe',
+			'Just a test recipe',
+			'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?fit=1200%2C879&ssl=1'
+		)
+	];
+
+	constructor() {}
+
+	ngOnInit(): void {}
+}
